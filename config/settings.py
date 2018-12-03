59a71be38c2ae4f83c394a134fbf45c9077675ca
Pavel Mametev <pavelmametev@yandex.ru>
2018-12-03 22:52:07 +0500

    Changes for deploy to Heroku

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'peaceful-escarpment-47177.herokuapp.com']
 
 
 # Application definition
@@ -126,6 +126,8 @@ TIME_FORMAT = 'H:i'
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 
